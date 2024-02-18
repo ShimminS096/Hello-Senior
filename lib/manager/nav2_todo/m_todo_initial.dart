1bea40d7c0cc773d0aee6ceb23f997470e8198bf
yehyeon <101814690+yeahyeon@users.noreply.github.com>
2024-02-18 10:49:15 +0900

    Update m_todo_initial.dart
    양예현이 이 파일 변경함
    투두리스트 작업 완료로 인한 파일 변경

--- lib/manager/nav2_todo/m_todo_initial.dart
+++ lib/manager/nav2_todo/m_todo_initial.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:knockknock/components/color.dart';
 import 'package:knockknock/manager/m_components/m_app_bar.dart';
 import 'package:knockknock/manager/nav2_todo/m_todo_box.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ManagerTodoInitial extends StatefulWidget {
   final int numberofSeniors;
-  const ManagerTodoInitial({super.key, required this.numberofSeniors});
+  final String currentUserUID; // currentUser의 UID를 저장할 변수 추가
+
+  const ManagerTodoInitial({
+    Key? key,
+    required this.numberofSeniors,
+    required this.currentUserUID, // 생성자에 currentUserUID 추가
+  }) : super(key: key);
 
   @override
   State<ManagerTodoInitial> createState() => _ManagerTodoInitialState();
 }
 
 class _ManagerTodoInitialState extends State<ManagerTodoInitial> {
-  final int numberofSeniors = 10;
   late ScrollController _scrollController;
 
   @override
@@ -27,41 +33,111 @@ class _ManagerTodoInitialState extends State<ManagerTodoInitial> {
     super.dispose();
   }
 
+  Future<List<String>> _getSeniorUIDs() async {
+    try {
+      final docSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc('B0z8CS40r7dtESumdeohkL0Rqyk2') // 현재 사용자의 UID 사용
+          .get();
+
+      if (docSnapshot.exists) {
+        final data = docSnapshot.data() as Map<String, dynamic>;
+        final seniorUIDs = List<String>.from(data['seniorUIDs']);
+        return seniorUIDs;
+      }
+    } catch (e) {
+      print("Error getting seniorUIDs: $e");
+    }
+
+    return []; // 오류 발생 시 빈 배열 반환
+  }
+
+  Future<String> _getSeniorName(String seniorUID) async {
+    try {
+      final docSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(seniorUID)
+          .get();
+
+      if (docSnapshot.exists) {
+        final data = docSnapshot.data() as Map<String, dynamic>;
+        final seniorName = data['name'] as String;
+        return seniorName;
+      }
+    } catch (e) {
+      print("Error getting senior name: $e");
+    }
+
+    return 'Unknown'; // 기본값으로 Unknown을 반환
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: MyColor.myBackground,
-      appBar: const ManagerAppBar(
-        title: "할 일",
-        size: 95,
-      ),
-      body: Scrollbar(
-        controller: _scrollController,
-        trackVisibility: true,
-        thumbVisibility: true,
-        thickness: 5,
-        radius: const Radius.circular(20),
-        child: ListView.builder(
-          controller: _scrollController,
-          shrinkWrap: true,
-          itemCount: numberofSeniors,
-          itemBuilder: (BuildContext context, int index) {
-            return const Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                  child: ToDoBox(
-                    name: '이름입력',
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                )
-              ],
-            );
-          },
-        ),
-      ),
+    return FutureBuilder<List<String>>(
+      future: _getSeniorUIDs(),
+      builder: (context, AsyncSnapshot<List<String>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+        if (snapshot.hasError || !snapshot.hasData) {
+          return const Text('Error');
+        }
+
+        final seniorUIDs = snapshot.data!;
+
+        return Scaffold(
+          backgroundColor: MyColor.myBackground,
+          appBar: const ManagerAppBar(
+            title: "할 일",
+            size: 95,
+          ),
+          body: Scrollbar(
+            controller: _scrollController,
+            trackVisibility: true,
+            thumbVisibility: true,
+            thickness: 5,
+            radius: const Radius.circular(20),
+            child: ListView.builder(
+              controller: _scrollController,
+              shrinkWrap: true,
+              itemCount: seniorUIDs.length,
+              itemBuilder: (BuildContext context, int index) {
+                final seniorUID = seniorUIDs[index];
+
+                return FutureBuilder<String>(
+                  future: _getSeniorName(seniorUID),
+                  builder: (context, AsyncSnapshot<String> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    }
+                    if (snapshot.hasError) {
+                      return const Text('Error');
+                    }
+
+                    final seniorName = snapshot.data ?? 'Unknown';
+
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 0,
+                            vertical: 0,
+                          ),
+                          child: ToDoBox(
+                            name: seniorName,
+                            seniorUID: seniorUID,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
