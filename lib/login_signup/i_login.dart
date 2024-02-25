c7197d75868388842e62bfd74cced1bc1dc84ac5
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2024-02-25 19:33:03 +0900

    Update i_login.dart

--- lib/login_signup/i_login.dart
+++ lib/login_signup/i_login.dart
@@ -85,7 +85,7 @@ class _LoginState extends State<Login> {
     try {
       UserCredential userCredential = await FirebaseAuth.instance
           .signInWithEmailAndPassword(
-              email: _phoneController.text + '@knockknock.com',
+              email: _phoneController.text + '@knockknock.mail',
               password: _phoneController.text);
 
       //로그인하려는 유저가 Authentication에 존재하는 경우
